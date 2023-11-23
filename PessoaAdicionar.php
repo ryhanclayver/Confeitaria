01902896f93b794188086a0e92f5796308e6c496
ryhanclayver <ryhan.clayver@hotmail.com>
2023-11-23 00:04:18 -0300

    insert no cadastro
    criei o arquivo PessoaAdicionar para fazer a conexão com o banco de dados até o cadastro

---
+++ PessoaAdicionar.php
@@ -0,0 +1,18 @@
+<?php
+	
+    $nome = $_POST['nome'];
+    $data_nascimento = date('Y-m-d', strtotime($_POST['data_nascimento']));
+    $cpf = $_POST['cpf'];
+    $endereco = $_POST['endereco'];
+    $sexo = $_POST['sexo'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    
+	
+
+	if(isset($_POST['nome'])){
+		require 'run.php';
+		$pessoa = new Pessoa();
+		$id_pessoa = $pessoa->adicionar($nome,$data_nascimento,$cpf,$endereco,$sexo,$email,$senha);
+	}
+    exit;
--- index.php
+++ index.php
@@ -28,23 +28,24 @@
             <!-- Formulário de Cadastro -->
             <div class="col-md-6">
                 <h3>Cadastre-se</h3>
-                <form>
+                <form method="POST" action="PessoaAdicionar.php">
                     <div class="mb-3">
-                        <label for="cadastroNome" class="form-label">Nome:</label>
-                        <input type="text" class="form-control" id="cadastroNome" placeholder="Seu nome">
+                        <label for="nome" class="form-label">Nome:</label>
+                        <input type="text" name="nome" id="nome" class="form-control" required placeholder="Seu nome">
                     </div>
                     <div class="row mb-3">
                     <div class="col">
-                            <label for="cadastroDataNascimento" class="form-label">Data de Nascimento:</label>
-                            <input type="date" class="form-control" id="cadastroDataNascimento">
+                            <label for="data_nascimento" class="form-label">Data de Nascimento:</label>
+                            <input type="date" class="form-control" id="data_nascimento" name="data_nascimento" required>
+
                         </div>
                         <div class="col">
-                            <label for="cadastroCpf" class="form-label">CPF:</label>
-                            <input type="text" class="form-control" id="cadastroCpf" placeholder="Seu CPF">
+                            <label for="cpf" class="form-label">CPF:</label>
+                            <input type="text" class="form-control" name="cpf" id="cpf" required placeholder="Seu CPF">
                         </div>
                         <div class="col">
-                            <label for="cadastroSexo" class="form-label">Sexo:</label>
-                            <select class="form-select" id="cadastroSexo">
+                            <label for="sexo" class="form-label">Sexo:</label>
+                            <select class="form-select" name="sexo" id="sexo" required>
                                 <option value="masculino">Masculino</option>
                                 <option value="feminino">Feminino</option>
                                 <option value="outros">Outros</option>
@@ -52,16 +53,16 @@
                         </div>
                     </div>
                     <div class="mb-3">
-                        <label for="cadastroEndereco" class="form-label">Endereço:</label>
-                        <input type="text" class="form-control" id="cadastroEndereco" placeholder="Seu endereço">
+                        <label for="endereco" class="form-label">Endereço:</label>
+                        <input type="text" class="form-control" id="endereco" name="endereco" required placeholder="Seu endereço">
                     </div>
                     <div class="mb-3">
-                        <label for="cadastroEmail" class="form-label">Email:</label>
-                        <input type="email" class="form-control" id="cadastroEmail" placeholder="Seu email">
+                        <label for="email" class="form-label">Email:</label>
+                        <input type="email" class="form-control" id="email" name ="email" required placeholder="Seu email">
                     </div>
                     <div class="mb-3">
-                        <label for="cadastroSenha" class="form-label">Senha:</label>
-                        <input type="password" class="form-control" id="cadastroSenha" placeholder="Sua senha">
+                        <label for="senha" class="form-label">Senha:</label>
+                        <input type="password" class="form-control" id="senha" name="senha" required placeholder="Sua senha">
                     </div>
                     <button type="submit" class="btn btn-success">Cadastrar</button>
                 </form>
