03203d354f9924bf02d7fed7339fb5a21285dcf7
ryhanclayver <ryhan.clayver@hotmail.com>
2023-11-24 16:43:15 -0300

    footer concluido

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 <footer>
-	
+	<p>Sweet Home Confeitaria<br>
+    <p>Rua Armando Constantino - Bairro Cristalino
+	<p>Catanduva - SP, CEP: 13603-055<br>
+    <p>(17) 99194 1481</p>	
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </footer>
