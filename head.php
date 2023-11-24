1368288ed52b3b9199e1b95c062e49bf1600dad2
ryhanclayver <ryhan.clayver@hotmail.com>
2023-11-24 10:04:02 -0300

    Melhoria interface

--- head.php
+++ head.php
@@ -8,6 +8,22 @@
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	</head>
 	<header>
-	<a>oi</a>
+
+		<div class="circle-container" id="circlesContainer"></div>
+		<script>
+			const circleContainer = document.getElementById('circlesContainer');
+			const circleWidth = 20;
+			const spacing = 10;
+			const screenWidth = window.innerWidth;
+			const numCirculos = Math.floor(screenWidth / (circleWidth + spacing));
+
+			for (let i = 0; i < numCirculos; i++) {
+				const circleDiv = document.createElement('div');
+				circleDiv.className = 'image-div';
+				circleContainer.appendChild(circleDiv);
+			}
+		</script>
+		<h1> oi </h1>
 	</header>
+	
 </html>
