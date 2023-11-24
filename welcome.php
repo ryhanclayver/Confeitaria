a923c5400e16b60d5f860c2753b50b8ae3e4ae3d
ryhanclayver <ryhan.clayver@hotmail.com>
2023-11-24 16:35:34 -0300

    welcome interface finalizada quase

--- welcome.php
+++ welcome.php
@@ -5,11 +5,14 @@
 
     <main>
         <div id="mural">
-
             <h1>Boas-vindas à SweetHome</h1>
             <h1>Confeitaria</h1>
-
         </div>
+        <div id="botoes" class="text-center">
+            <button type="button" class="btn btn-primary mt-7 mb-6" >Cadastrar Produtos</button>
+            <button type="button" class="btn btn-success mt-7 mb-6">Comprar Produtos</button>
+        </div>
+    <?php require 'footer.php'; ?>
     </main>
     </div>
 </html>
