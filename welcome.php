21fc6d9825f2b6e79850760640db046af4aff91e
ryhanclayver <ryhan.clayver@hotmail.com>
2023-11-27 13:44:00 -0300

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -10,7 +10,7 @@
             </div>
             <div id="botoes" class="text-center">
                 <button type="button" class="btn btn-primary mt-7 mb-6" onclick="redirecionarCadastroProdutos()" >Cadastrar Produtos</button>
-                <button type="button" class="btn btn-success mt-7 mb-6">Comprar Produtos</button>
+                <button type="button" class="btn btn-success mt-7 mb-6" onclick="redirecionarComprarProdutos()">Comprar Produtos</button>
             </div>
             <?php require 'footer.php'; ?>
         </main>
@@ -19,6 +19,10 @@
         function redirecionarCadastroProdutos() {
             // Redireciona para cadastroprodutos.php
             window.location.href = 'cadastroProdutos.php';
+        }
+        function redirecionarComprarProdutos() {
+            // Redireciona para comprarprodutos.php
+            window.location.href = 'comprarProdutos.php';
         }
     </script>
 </html>
